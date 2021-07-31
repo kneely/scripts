ccd5f0058291d873e278274ac3480162cfcefd1a
Kevin Neely <37545028+kneely@users.noreply.github.com>
2021-07-31 08:15:21 -0400

    Update wifi-to-eth.sh

--- wifi-to-eth.sh
+++ wifi-to-eth.sh
@@ -12,7 +12,7 @@
 # Don't forget to change the name of network interface
 # Check them with `ifconfig`
 
-ip_address="192.168.2.1"
+ip_address="192.168.20.1"
 netmask="255.255.255.0"
 dhcp_range_start="192.168.2.2"
 dhcp_range_end="192.168.2.100"
